610966ab3d26294af8d6692d84f37710483e6e71
Andrzej Hanusek <andrzej.hanusek@tsh.io>
2020-07-16 11:15:37 +0200

    feat: enable Hermes for android

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+        extensions: ['.js', '.ts', '.tsx', 'd.ts', '.jsx'],
+      },
+    ],
+  ],
 };
