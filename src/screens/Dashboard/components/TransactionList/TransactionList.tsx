057d6088818e3d32419b3284b2d88b23b20ac2b4
Andrzej Hanusek <hanusek.a@gmail.com>
2020-07-16 14:29:03 +0200

    finish basic component task

---
+++ src/screens/Dashboard/components/TransactionList/TransactionList.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+import { View } from 'react-native';
+import { transactions } from '@/screens/Dashboard/components/TransactionList/mockData';
+import { BudgetCategoryItem } from '@/screens/Dashboard/components';
+
+type OwnProps = {};
+
+export type TransactionListProps = OwnProps;
+
+const TransactionList: FunctionComponent<TransactionListProps> = () => {
+  return (
+    <View>
+      {transactions.map((transaction) => (
+        <BudgetCategoryItem {...transaction} key={transaction.id} />
+      ))}
+    </View>
+  );
+};
+
+export default TransactionList;
