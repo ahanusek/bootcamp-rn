3024a8a2a07c0136db8a26d4aefb5ee8db2c2c82
Andrzej Hanusek <hanusek.a@gmail.com>
2020-10-14 20:45:41 +0200

    refactor icons & splash screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import SplashScreen from '@/screens/SplashScreen/SplashScreen';
 import MainNavigator from '@/routes/Main/MainNavigator';
 import { Host } from 'react-native-portalize';
 import { palette } from '@/theme';
+import AuthNavigator from '@/routes/Auth/AuthNavigator';
 
 const MyTheme = {
   ...DefaultTheme,
@@ -15,25 +15,24 @@ const MyTheme = {
 };
 
 const App = () => {
-  const [auth, updateAuthState] = useState({
+  const [auth, updateAuthState] = useState<{
+    isLoading: boolean;
+    userToken: string | null;
+  }>({
     isLoading: true,
     userToken: null,
   });
   useEffect(() => {
     setTimeout(() => {
-      updateAuthState({ isLoading: false, userToken: null });
+      updateAuthState({ isLoading: false, userToken: '124' });
     }, 3000);
   }, []);
-
-  if (auth.isLoading) {
-    return <SplashScreen />;
-  }
 
   return (
     <SafeAreaProvider>
       <Host>
         <NavigationContainer theme={MyTheme}>
-          <MainNavigator />
+          {auth.userToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       </Host>
     </SafeAreaProvider>
