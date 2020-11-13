a2dc4ac183f46c19b1530a6a5df61d435f0637f9
Andrzej Hanusek <hanusek.a@gmail.com>
2020-11-13 11:18:17 +0100

    add opacity animation

--- src/screens/Cards/Cards.tsx
+++ src/screens/Cards/Cards.tsx
@@ -1,15 +1,62 @@
-import React, { FunctionComponent } from 'react';
-import { SafeAreaView } from 'react-native';
-import { ScreenTitle } from '@/components';
+import React, { FunctionComponent, useRef } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { ScreenTitle, Spacer } from '@/components';
+import { appStyles } from '@/theme';
 
 type OwnProps = {};
 
 export type CardsProps = OwnProps;
 
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#FF0000',
+  },
+  container: {
+    flex: 1,
+  },
+});
+
 const Cards: FunctionComponent<CardsProps> = () => {
+  const animatedValue = useRef(new Animated.Value(1)).current;
+
+  const startAnimation = () => {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 350,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    });
+  };
+
+  const animatedStyles = {
+    opacity: animatedValue,
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <ScreenTitle title="Cards" />
+      <Animated.View style={[appStyles.centered, animatedStyles]}>
+        <TouchableWithoutFeedback onPress={startAnimation}>
+          <View style={styles.box} />
+        </TouchableWithoutFeedback>
+        <Spacer size="large" />
+        <TouchableWithoutFeedback onPress={startAnimation}>
+          <View style={styles.box} />
+        </TouchableWithoutFeedback>
+      </Animated.View>
     </SafeAreaView>
   );
 };
