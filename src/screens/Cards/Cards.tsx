dcf3ead6f157c4feaacb79da7f77265cb207816f
Andrzej Hanusek <hanusek.a@gmail.com>
2020-11-13 11:33:43 +0100

    add bg interpolation

--- src/screens/Cards/Cards.tsx
+++ src/screens/Cards/Cards.tsx
@@ -26,6 +26,21 @@ const styles = StyleSheet.create({
 
 const Cards: FunctionComponent<CardsProps> = () => {
   const animatedValue = useRef(new Animated.Value(1)).current;
+  const transformAnimation = useRef(new Animated.Value(0)).current;
+
+  const startTransformAnimation = () => {
+    Animated.timing(transformAnimation, {
+      toValue: 100,
+      duration: 350,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(transformAnimation, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    });
+  };
 
   const startAnimation = () => {
     Animated.timing(animatedValue, {
@@ -45,18 +60,40 @@ const Cards: FunctionComponent<CardsProps> = () => {
     opacity: animatedValue,
   };
 
+  const transformAnimatedStyles = {
+    transform: [
+      {
+        translateY: transformAnimation,
+      },
+      {
+        translateX: transformAnimation,
+      },
+    ],
+  };
+
+  const bgInterpolation = transformAnimation.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['rgb(255, 99, 71)', 'rgb(99, 71, 255)'],
+  });
+
   return (
     <SafeAreaView style={styles.container}>
       <ScreenTitle title="Cards" />
-      <Animated.View style={[appStyles.centered, animatedStyles]}>
+      <View style={appStyles.centered}>
         <TouchableWithoutFeedback onPress={startAnimation}>
-          <View style={styles.box} />
+          <Animated.View style={[styles.box, animatedStyles]} />
         </TouchableWithoutFeedback>
         <Spacer size="large" />
-        <TouchableWithoutFeedback onPress={startAnimation}>
-          <View style={styles.box} />
+        <TouchableWithoutFeedback onPress={startTransformAnimation}>
+          <Animated.View
+            style={[
+              styles.box,
+              transformAnimatedStyles,
+              { backgroundColor: bgInterpolation },
+            ]}
+          />
         </TouchableWithoutFeedback>
-      </Animated.View>
+      </View>
     </SafeAreaView>
   );
 };
